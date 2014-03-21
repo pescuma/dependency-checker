3b27ce2b38589a08dcb09352816caff47bebafc2
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2014-03-21 14:41:30 -0300

    Fixed version numnbers and copyright lines in AssemblyInfos

--- DependencyChecker.Cli/Properties/AssemblyInfo.cs
+++ DependencyChecker.Cli/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DependencyChecker.Cli")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Ricardo Pescuma Domenecci 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
